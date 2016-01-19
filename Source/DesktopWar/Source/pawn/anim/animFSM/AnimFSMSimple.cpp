3d60a39b26f9a1f543f7679cf41218d1ba7939da
HawardLocke <1534921818@qq.com>
2016-01-19 21:01:16 +0800

    change state

--- Source/DesktopWar/Source/pawn/anim/animFSM/AnimFSMSimple.cpp
+++ Source/DesktopWar/Source/pawn/anim/animFSM/AnimFSMSimple.cpp
@@ -23,5 +23,34 @@ void AnimFSMSimple::Initialize()
 
 void AnimFSMSimple::DoAction(PawnAction* pAction)
 {
+	if (CurrentAnimState.HandleNewAction(action) == true)
+	{
+		//Debug.Log("AC - Do Action " + action.ToString());
+		NextAnimState = null;
+	}
+	else
+	{
+		if (action is AgentActionGoTo)
+			NextAnimState = AnimStates[(int)E_MyAnimState.gotoPos];
+		if (action is AgentActioCombatMove)
+			NextAnimState = AnimStates[(int)E_MyAnimState.combatMove];
+		else if (action is AgentActionRotate)
+			NextAnimState = AnimStates[(int)E_MyAnimState.Rotate];
+		else if (action is AgentActionAttack)
+			NextAnimState = AnimStates[(int)E_MyAnimState.attack];
+		else if (action is AgentActionWeaponShow)
+			NextAnimState = AnimStates[(int)E_MyAnimState.idle];
+		else if (action is AgentActionPlayAnim || action is AgentActionPlayIdleAnim)
+			NextAnimState = AnimStates[(int)E_MyAnimState.playAnim];
+		else if (action is AgentActionInjury)
+			NextAnimState = AnimStates[(int)E_MyAnimState.injury];
+		else if (action is AgentActionKnockdown)
+			NextAnimState = AnimStates[(int)E_MyAnimState.knockdown];
+		else if (action is AgentActionDeath)
+			NextAnimState = AnimStates[(int)E_MyAnimState.death];
 
+
+		ProgressToNextStage(action);
+
+	}
 }
